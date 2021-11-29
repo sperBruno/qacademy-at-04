1ca4544e446b79385226cb31b320363fb1b72b97
Bruno Barrios <bruno.barrios@socialive.us>
2021-11-29 19:23:34 -0400

    Added hooks to my code

--- src/bruno.spec.js
+++ src/bruno.spec.js
@@ -1,9 +1,45 @@
-test("Sum of two digits", ()=>{
-    // Arrange
+beforeAll(() => {
+  console.log("before all hook");
+});
 
-    // Act
-    let sum = 2 + 2;
-    
-    // Assert
-    expect(sum).toBe(4);
+beforeEach(() => {
+  console.log("Before each hook");
+});
+test("Sum of two digits", () => {
+  // Arrange
+
+  // Act
+  let sum = 2 + 2;
+
+  // Assert
+  expect(sum).toBe(4);
+  console.log("Test suma bruno");
+});
+
+test("Sum of two digits2", () => {
+  // Arrange
+
+  // Act
+  let sum = 2 + 2;
+
+  // Assert
+  expect(sum).toBe(4);
+  console.log("Test suma bruno 2");
+});
+test("Sum of two digits3", () => {
+  // Arrange
+
+  // Act
+  let sum = 2 + 2;
+
+  // Assert
+  expect(sum).toBe(4);
+  console.log("Test suma bruno 3");
+});
+
+afterEach(() => {
+  console.log("After Each hook");
+});
+afterAll(() => {
+  console.log("After all hook");
 });
